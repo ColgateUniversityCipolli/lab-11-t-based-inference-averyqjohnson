9a5cba412ed194fc681436bafb9f5f419de866f6
averyqjohnson <aqjohnson@colgate.edu>
2025-04-08 15:51:11 -0400

    Through Step 4

--- Lab11Code.R
+++ Lab11Code.R
@@ -66,3 +66,92 @@ ggplot(data=dat.long, aes(x=condition, y=fluorescence)) +
   ylab("% Change in Fluorescence")
 
 # optional challenge: reproduce figure 2(g)
+
+################################################################################
+# Question 4: conduct inferences
+################################################################################
+library(effectsize)
+
+# close
+hedges_result_closer <- hedges_g(x = dat.clean$closer_vals, mu = 0, alternative = "greater")
+interpret_hedges_g(1.61)
+
+help("hedges_g")
+
+t_test_result_closer <- t.test(x=dat.clean$closer_vals, mu = 0, alternative = "greater")
+t_test_result_closer.ci <- t.test(x=dat.clean$closer_vals, mu = 0, alternative = "two.sided")
+
+t_stat_closer <- t_test_result_closer$statistic
+p_val_closer <- t_test_result_closer$p.value
+hedges_g_closer <- hedges_result_closer$Hedges_g
+ci_low_closer <- t_test_result_closer.ci$conf.int[1]
+ci_high_closer <- t_test_result_closer.ci$conf.int[2]
+
+results.closer <- data.frame(
+  t = c(t_stat_closer),
+  p_value = c(p_val_closer),
+  g = c(hedges_g_closer),
+  CI_Lower = c(ci_low_closer),
+  CI_Upper = c(ci_high_closer)
+)
+
+view(results.closer)
+
+library(xtable)
+results.closer.xtable <- xtable(results.closer)
+
+# far
+hedges_result_further <- hedges_g(x = dat.clean$further_vals, mu = 0, alternative = "less")
+interpret_hedges_g(-1.51)
+
+t_test_result_further <- t.test(x=dat.clean$further_vals, mu = 0, alternative = "less")
+t_test_result_further.ci <- t.test(x=dat.clean$further_vals, mu = 0, alternative = "two.sided")
+
+t_stat_further <- t_test_result_further$statistic
+p_val_further <- t_test_result_further$p.value
+hedges_g_further <- hedges_result_further$Hedges_g
+ci_low_further <- t_test_result_further.ci$conf.int[1]
+ci_high_further <- t_test_result_further.ci$conf.int[2]
+
+results.further <- data.frame(
+  t = c(t_stat_further),
+  p_value = c(p_val_further),
+  g = c(hedges_g_further),
+  CI_Lower = c(ci_low_further),
+  CI_Upper = c(ci_high_further)
+)
+
+view(results.further)
+
+library(xtable)
+results.further.xtable <- xtable(results.further)
+
+# differences
+hedges_result_difference <- hedges_g(x = dat.clean$difference, mu = 0, alternative = "two.sided")
+interpret_hedges_g(1.65)
+
+t_test_result_difference <- t.test(x=dat.clean$difference, mu = 0, alternative = "two.sided")
+
+t_stat_difference <- t_test_result_difference$statistic
+p_val_difference <- t_test_result_difference$p.value
+hedges_g_difference <- hedges_result_difference$Hedges_g
+ci_low_difference <- t_test_result_difference$conf.int[1]
+ci_high_difference <- t_test_result_difference$conf.int[2]
+
+results.difference <- data.frame(
+  t = c(t_stat_difference),
+  p_value = c(p_val_difference),
+  g = c(hedges_g_difference),
+  CI_Lower = c(ci_low_difference),
+  CI_Upper = c(ci_high_difference)
+)
+
+view(results.difference)
+
+library(xtable)
+results.difference.xtable <- xtable(results.difference)
+
+################################################################################
+# Question 5: reverse engineer hypothesis test plot from lecture 20
+################################################################################
+
